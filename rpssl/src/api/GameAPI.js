7188dfa48b69234401833a829b872dee0fb72b4c
Altemush Bhatti <altemush@gmail.com>
2022-04-23 14:17:35 -0700

    feat: create Game API methods

---
+++ rpssl/src/api/GameAPI.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const api = 'https://codechallenge.boohma.com';
+
+export const getAllChoices = () =>
+  axios.get(`${api}/choices`).then((response) => response);
+
+export const getRandomChoice = () =>
+  axios.get(`${api}/choice`).then((response) => response);
+
+export const playRound = (data) =>
+  axios.post(`${api}/play`, data).then((response) => {
+    console.log(response);
+    console.log(response.data);
+    return response;
+  });
