90c3a93cb6652dc3e1af241f88831a28ea0fa8cf
Altemush Bhatti <altemush@gmail.com>
2022-04-24 18:05:05 -0700

    feat: created reusable choice component

--- rpssl/src/App.js
+++ rpssl/src/App.js
@@ -1,8 +1,16 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable react/jsx-curly-brace-presence */
 import React from 'react';
 import './App.css';
+import Choice from './components/input/Choice';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Choice />
+    </div>
+  );
 }
 
 export default App;
---
+++ rpssl/src/components/input/Choice.js
@@ -0,0 +1,24 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from '@material-ui/core';
+
+function Choice({ choice }) {
+  return (
+    <Button>
+      <img
+        src={require(`../../assets/choices/${choice}.png`)}
+        alt={choice}
+        width="300"
+        height="300"
+      />
+    </Button>
+  );
+}
+
+Choice.propTypes = {
+  choice: PropTypes.string.isRequired,
+};
+
+export default Choice;
